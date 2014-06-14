e732747981ab57194b3cd6a559176dd5d02e2343
Terry Peppers <peppers@gmail.com>
2014-06-13 20:38:21 -0500

    Between you and me, I think the ternary opeerator is kind of bullshit. Shhhh.

---
+++ 02_exploring_callbacks.js
@@ -0,0 +1,27 @@
+
+'use strict';
+
+var obj = function () { };
+
+obj.prototype.doSomething = function(arg1, arg2) {
+	var arg2 = typeof(arg2) === 'string' ? arg2 : null;
+	var callback = arguments[arguments.length - 1];
+	callback = (typeof(callback)) === 'function' ? callback : null;
+	if (!arg2) {
+		return callback(new Error('second arg missing or not string'));
+	}
+	callback(arg1);
+}
+
+var test = new obj();
+
+try {
+	test.doSomething('test', '3.55', function(err, value) {
+		if (err) {
+			throw err;
+		}
+		console.log(value)
+	});
+} catch (err) {
+	console.error(err);
+}
