f09c9b24df0a0c79dba017401ff0c0adf9cd0d01
Terry Peppers <terryp@leapfrogonline.com>
2014-06-16 08:33:46 -0500

    Credit where credit is due.

--- learning/sp_ln_5-3_synchronous_fs.js
+++ learning/sp_ln_5-3_synchronous_fs.js
@@ -1,3 +1,11 @@
+
+/*
+
+This is unoriginal thought. Basically, I'm working through some execises in 
+Shelly Powers' 'Leaning Node' book - specically Chapter 5 / Control Flow.
+
+*/
+
 'use strict';
 
 var fs = require ('fs');
--- learning/sp_ln_5-5_aynchronous_file_manipulation.js
+++ learning/sp_ln_5-5_aynchronous_file_manipulation.js
@@ -1,4 +1,11 @@
 
+/*
+
+This is unoriginal thought. Basically, I'm working through some execises in 
+Shelly Powers' 'Leaning Node' book - specically Chapter 5 / Control Flow.
+
+*/
+
 var fs = require('fs');
 var util = require('util');
 
