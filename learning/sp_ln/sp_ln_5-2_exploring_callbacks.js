efe570c5b6d2b31a52d45b51320e96887b2ebf0b
Terry Peppers <terryp@leapfrogonline.com>
2014-06-16 08:38:09 -0500

    Credit where credit is due.

--- learning/sp_ln/sp_ln_5-2_exploring_callbacks.js
+++ learning/sp_ln/sp_ln_5-2_exploring_callbacks.js
@@ -1,3 +1,9 @@
+/*
+
+This is unoriginal thought. Basically, I'm working through some execises in 
+Shelly Powers' 'Leaning Node' book - specically Chapter 5 / Control Flow.
+
+*/
 
 'use strict';
 
