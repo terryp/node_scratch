c251a8fc6c42d9c1fc28e9152ef34e7fb2b68aa0
Terry Peppers <terryp@leapfrogonline.com>
2014-06-17 07:56:54 -0500

    Conflict resolution.

--- learning/sandbox/playing_with_functions/01_playing_with_functions.js
+++ learning/sandbox/playing_with_functions/01_playing_with_functions.js
@@ -5,7 +5,6 @@
 
 You're not going to understand callbacks, if you don't understand functions!
 
-<<<<<<< HEAD
 First things first ... declaration and invocation. 
 
 */
@@ -18,17 +17,3 @@ greeting();								// This is invocation. We have to
 										// call the function and basically say,
 										// "Do your thing!" So it does, it
 										// prints 'Hello, World!'
-=======
-The difference between declaration and invocation.
-
-*/
-
-function greeting() {				// This is declaring a function.
-	console.log('Hello, World!');	// It's named greeting.
-}									// It prints 'Hello, World!'
-
-greeting();							// This is invocation. 
-									// See, if we just declare the function
-                                    // it doesn't do anything. This does
-                                    // something.
->>>>>>> 7490e120b93bd830542dfa2f48de90114b902601
