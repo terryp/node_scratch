c37910823561229a581b630a847a2a77660b506c
Terry Peppers <terryp@leapfrogonline.com>
2014-06-17 07:57:59 -0500

    Conflict resolution.

--- learning/sandbox/playing_with_functions/02_playing_with_functions.js
+++ learning/sandbox/playing_with_functions/02_playing_with_functions.js
@@ -5,13 +5,12 @@
 
 You're not going to understand callbacks, if you don't understand functions!
 
-<<<<<<< HEAD
 Second, functions that take arguments as parameters!
 
 */
 
-function greeting(string) {				// This is declaration. We create
-	console.log('Hello, ' + string);	// a new function and name it greeting.
+function greeting(s) {					// This is declaration. We create
+	console.log('Hello, ' + s + '!');	// a new function and name it greeting.
 }										// This time, however, we change the 
 										// function so that it can take in a
 										// string and print out 
@@ -27,19 +26,4 @@ greeting('JavaScript');					// This is invocation but now we have
 										// the string JavaScript so the 
 										// function will return 
 										// 'Hello, JavaScript'
-=======
-Arguments and functions.
 
-*/
-
-function greeting(s) {                  // Same function as before, but this
-    console.log('Hello, ' + s + '!');   // now takes an argument - 
-}                                       // specifically a string. 
-
-greeting()                              // Little trick here, since no                       
-                                        // argument is passed and a default
-                                        // argument isn't supplied this will 
-                                        // say 'Hello, undefined!'
-
-greeting('Argument');                   // This works as we would expect.                                  
->>>>>>> 7490e120b93bd830542dfa2f48de90114b902601
