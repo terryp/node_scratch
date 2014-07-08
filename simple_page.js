8440556429d5038d80bf7dd4dbc9a542faa52051
Terry Peppers <terryp@leapfrogonline.com>
2014-07-08 17:14:25 -0500

    Ah, fancy turd polishing.

--- simple_page.js
+++ simple_page.js
@@ -1,9 +1,18 @@
 
 'use strict';
 
+// Basic imports. 
+// async - Do things asynchronously
+// cheerio - Fast parser using jQuery selectors
+// request - Wrapper to make requests and parse responses
+
 var async = require('async');
 var cheerio = require('cheerio');
 var request = require('request');
+
+// Basic Page class. 
+// Pulls in a response and a body and uses those elements in tandem with
+// cheerio to set basic properties of the class itself. 
 
 var Page = function(res, body) {
     this.response = res;
@@ -25,6 +34,10 @@ var Page = function(res, body) {
     }
 }
 
+// This is actually the heart of the matter. It's a function that will
+// accept a URL parameter, go get it and then create a simple Page object
+// and then print out the description of the page. 
+
 function get(url) {
     console.log("\nGoing to " + url);
     request(url, function (err, res, body) {
@@ -36,6 +49,8 @@ function get(url) {
     });
 }
 
+// Simple array of URLs
+
 var urls = [
     "http://www.google.com",
     "http://www.yahoo.com",
@@ -43,7 +58,10 @@ var urls = [
     "http://www.chicagotribune.com",
 ]
 
+// Ah ha! Some wild and crazy stuff. Asynchronously run through the list of
+// URLs and pass in the get function.  
+
 async.each(urls, get, function(err) {
-    console.log("\n");
+    if (err) throw err;
 })
 
