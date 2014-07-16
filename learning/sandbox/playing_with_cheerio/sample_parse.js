b5888f8d89d6b50016ee361682f90599501c584b
Terry Peppers <peppers@gmail.com>
2014-07-16 06:56:57 -0500

    Functionizing

--- learning/sandbox/playing_with_cheerio/sample_parse.js
+++ learning/sandbox/playing_with_cheerio/sample_parse.js
@@ -7,13 +7,15 @@ var cheerio = require('cheerio');
 var raw = fs.readFileSync('./index.html').toString();
 var html = cheerio.load(raw);
 
-var links = {}
+var getLinks = function() {
+    var links = {};
+    html('a').each(function() {
+        var link = html(this);
+        var text = link.text();
+        var href = link.attr('href');
+        links[text] = href;
+    });
+    return links;
+}
 
-html('a').each(function() {
-    var link = html(this);
-    var text = link.text();
-    var href = link.attr('href');
-    links[text] = href;
-})
-
-console.log(links);
+console.log(getLinks());
