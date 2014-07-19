9f58343996b9741990ac97423656d066f9daa70b
Terry Peppers <peppers@gmail.com>
2014-07-18 21:05:20 -0500

    Now that I know how to use JS Hint in Sublime, I will code even slower.

--- learning/sandbox/playing_with_loops/01_playing_with_loops.js
+++ learning/sandbox/playing_with_loops/01_playing_with_loops.js
@@ -1,3 +1,4 @@
+/*jslint node: true */
 
 'use strict';
 
@@ -12,28 +13,28 @@ to do this.
 var items = [1,2,3,4];                          // Here's the target array
                                                 // that I'll be looping over.
 
-console.log("What Am I Looping Over?!");        // Log what you're doing.
+console.log('What Am I Looping Over?!');        // Log what you're doing.
 console.log(items);
-console.log("\n");
+console.log('\n');
 
-console.log ("Standard Looping");               // Here's the typical way of
+console.log ('Standard Looping');               // Here's the typical way of
 for (var i = 0; i < items.length; i++) {        // looping using a C style 
 	console.log(items[i] + 1);                  // counter. 
 }
-console.log("\n");
+console.log('\n');
 
-console.log ("For In Looping");                 // Here's another way to loop
+console.log ('For In Looping');                 // Here's another way to loop
 for (var i in items) {                          // using the for-in approach.
 	console.log(items[i] + 1);                  // This is more similar to how                 
 }                                               // I prefer to loop in Python.
-console.log("\n");
+console.log('\n');
 
 
-console.log("For Each Looping")                 // And yet another way! This
+console.log('For Each Looping');                // And yet another way! This
 items.forEach(function(i) {                     // is a for-each loop which
 	console.log(i + 1);                         // seems more similar to IIRC
 });                                             // a Ruby block or a Python
-console.log("\n");                              // list comprehension.
+console.log('\n');
 
 
 var async = require('async');                   // Now that we've taken a look
@@ -41,20 +42,20 @@ var async = require('async');                   // Now that we've taken a look
                                                 // this, how about using 
                                                 // external libs.
 
-console.log("Async Looping");                   // Still another way, async!
+console.log('Async Looping');                   // Still another way, async!
 var add_one = function(x, doneCallback) {       // Need to define a function
     console.log(x + 1);                         // in order for this to work.
     return doneCallback(null);                  // Well, you could use an
-}                                               // anonymous function but this
+};                                              // anonymous function but this
                                                 // seems a little more explicit.
 
-async.each(items, add_one, function(err) {
-	console.log("\n");
-})
+async.each(items, add_one, function() {
+	console.log('\n');
+});
 
 var _ = require('underscore');                  // What about the underscore.js?
 
-console.log("Underscore Looping - Each")
+console.log('Underscore Looping - Each');
 _.each(items, function(num) { 
     console.log(num + 1); 
 });
