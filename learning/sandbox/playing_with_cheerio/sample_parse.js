3c706945e63da9870e5463fe5f6aa55abe3c70ec
Terry Peppers <peppers@gmail.com>
2014-07-29 21:55:56 -0500

    Very rudimentary and I am certain idiotic ways of parsing form data. Much to refactor. Much idiocy.

--- learning/sandbox/playing_with_cheerio/sample_parse.js
+++ learning/sandbox/playing_with_cheerio/sample_parse.js
@@ -10,12 +10,65 @@ var request = require('request');
 var raw = fs.readFileSync('./index.html').toString();
 var html = cheerio.load(raw);
 
-var moreRaw = request('http://www.imdb.com', function(err, res, body) {
-    if (err) throw err;
-    return cheerio.load(body);
-});
+// var moreRaw = request('http://www.imdb.com', function(err, res, body) {
+//     if (err) throw err;
+//     return cheerio.load(body);
+// });
 
-console.log(moreRaw);
+// console.log(moreRaw);
+
+/* 
+
+What would a form look like in JSON?
+
+form = {
+    id: integer,
+    name: string,
+    action: string,
+    method: string, 
+    fields : { keys: values }
+    button : { keys: values }
+}
+
+*/
+
+var revisedGetForm = function() {
+    var forms = {};
+    
+    html('form').each(function() {
+        var form = html(this);
+        var name = form.attr('name');
+        var action = form.attr('action');
+        var method = form.attr('method')
+        forms[name] = {'action': action, 'method': method};
+    });
+
+    var fields = {};
+    
+    html('form input').each(function() {
+        var field = html(this);
+        var id = field.attr('id');
+        var type = field.attr('type');
+        var name = field.attr('name');
+        fields[id] = {'type': type, 'name': name};
+    });
+
+    forms.fields = fields;
+
+    var buttonDetail = {};
+
+    html('form button').each(function() {
+        var button = html(this);
+        var type = button.attr('type');
+        var text = button.text();
+        buttonDetail = {'type': type, 'text': text};
+    });
+
+    forms.button = buttonDetail;
+
+    return forms; 
+}
+
 
 var getLinks = function() {
     var links = {};
@@ -63,7 +116,9 @@ var getFormButton = function() {
     return buttonDetail;
 }
 
-console.log(getLinks());
-console.log(getForms());
-console.log(getFormFields());
-console.log(getFormButton());
+// console.log(getLinks());
+// console.log(getForms());
+// console.log(getFormFields());
+// console.log(getFormButton());
+
+console.log(revisedGetForm());
