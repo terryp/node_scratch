1df754ea7466f57ef61dfafeac8a3c4f35b7d66e
Terry Peppers <peppers@gmail.com>
2014-08-10 22:04:03 -0500

    NIA Sunday night.

---
+++ learning/nia/03/currency_app/currency.js
@@ -0,0 +1,17 @@
+/*jslint node: true */
+
+'use strict';
+
+var canadianDollar = 0.91;
+
+function roundTwoDecimals(amount) {
+    return Math.round(amount * 100) / 100;
+}
+
+exports.canadianToUS = function(canadian) {
+    return roundTwoDecimals(canadian * canadianDollar);
+}
+
+exports.USToCanadian = function(us) {
+    return roundTwoDecimals(us / canadianDollar);
+}
---
+++ learning/nia/03/currency_app/test-currency.js
@@ -0,0 +1,8 @@
+
+var currency = require('./currency.js');
+
+console.log('50 Canadian dollars is this many US dollars');
+console.log(currency.canadianToUS(50));
+
+console.log('30 US dollars is this many Canadian dollars');
+console.log(currency.USToCanadian(30));
