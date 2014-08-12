1f29f70d01284e85cc53ddc41cb52c078f162a16
Terry Peppers <peppers@gmail.com>
2014-08-11 21:38:09 -0500

    Hmm. Kind of got somewhere.

--- simple_post.js
+++ simple_post.js
@@ -2,15 +2,6 @@
 
 'use strict';
 
-/*
-Dear god this shoudn't be so fucking HARD!
-
-http://stackoverflow.com/questions/6158933/how-to-make-an-http-post-request-in-node-js
-
-http://stackoverflow.com/questions/16687618/how-do-i-get-the-redirected-url-from-the-nodejs-request-module
-*/
-
-var util = require('util');
 var request = require('request');
 var cheerio = require('cheerio');
 
@@ -20,7 +11,6 @@ request.get(targetUrl, function(err, res, body) {
     if (err) throw err;
     parsePage(body, res);
 });
-
 
 function parsePage(body, res) {
     var formDetail = {};
