2f15f9169b2d1cc28e94ccfe69601e1d0f20d473
Terry Peppers <peppers@gmail.com>
2014-08-26 21:08:46 -0500

    Finishing async.

---
+++ learning/nia/03/word_count/word_count.js
@@ -0,0 +1,58 @@
+/*jslint node: true */
+
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var completedTasks = 0;
+var tasks = [];
+var wordCounts = {};
+
+// Complex
+var here = path.resolve(__dirname);
+var filesDir = path.resolve(here, '../../../', 'data');
+
+// Simple
+// var filesDir = './text';
+
+function checkIfComplete() {
+    completedTasks++;
+    if (completedTasks == tasks.length) {
+        for (var index in wordCounts) {
+            console.log(index + ': ' + wordCounts[index]);
+        }
+    }
+}
+
+function countWordsInText(text) {
+    var words = text 
+        .toString()
+        .toLowerCase()
+        .split(/\W+/)
+        .sort();
+    for (var index in words) {
+        var word = words[index];
+        if (word) {
+            wordCounts[word] = (wordCounts[word]) ? wordCounts[word] + 1 : 1;
+        }
+    }
+}
+
+fs.readdir(filesDir, function(err, files) { 
+    if (err) throw err;
+    for(var index in files) {
+        var task = (function(file) { 
+            return function() {
+                fs.readFile(file, function(err, text) {
+                    if (err) throw err;
+                    countWordsInText(text);
+                    checkIfComplete();
+                });
+            }
+        })(filesDir + '/' + files[index]);
+        tasks.push(task); 
+    }
+    for(var task in tasks) { 
+        tasks[task]();
+    }
+});
