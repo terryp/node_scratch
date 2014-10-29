16fd63dd53928dc201cb50c306c4d5b18ec4ec71
Terry Peppers <peppers@gmail.com>
2014-10-28 21:10:44 -0500

    EOD commit

--- learning/nsa/http_server.js
+++ learning/nsa/http_server.js
@@ -12,9 +12,9 @@ var tr = through(function(buffer) {
 
 var server = http.createServer(function(req, res) {
     if (req.method === 'POST') {
-        var msg = tr(req.body);
+        var msg = res.pipe(tr)
+        res.write(msg);
     }
-    res.end(msg);
 });
 
 server.listen(process.argv[2]);
