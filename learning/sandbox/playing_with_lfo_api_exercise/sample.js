8c434d3c740a6052bb100cc867e242c5ac6fefc9
Terry Peppers <peppers@gmail.com>
2015-06-29 12:58:44 -0500

    Ugh.

--- learning/sandbox/playing_with_lfo_api_exercise/sample.js
+++ learning/sandbox/playing_with_lfo_api_exercise/sample.js
@@ -6,16 +6,12 @@ var http = require('http');
 
 var request = require('request');
 
-var app = http.createServer(function(req, res) {
+var server = http.createServer(function(req, res) {
     res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(
-        {
-            a : 1
-        }
-    ));
-})
+    res.send(JSON.stringify({ a : 1 }));
+});
 
-app.listen(3000);
+server.listen(3000);
 
 // request('http://127.0.0.1', function(err, resp, body) {
 //     if (!err && response.statusCode == 200) {
