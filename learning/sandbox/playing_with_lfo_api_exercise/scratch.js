2d12b391d2233f889b45808e679334680cf5d6db
Terry Peppers <terryp@leapfrogonline.com>
2015-07-01 10:34:38 -0500

    Maybe

--- learning/sandbox/playing_with_lfo_api_exercise/scratch.js
+++ learning/sandbox/playing_with_lfo_api_exercise/scratch.js
@@ -54,3 +54,96 @@ request('http://127.0.0.1:3000/200.html', function(err, res, body) {
 //  - 400 / We get some kind of missing file or resource, we're maybe not good. 
 //  - 500 / We get some kind of server error, not good
 //  - Panic / We get zilcho
+
+// Older Scratch
+
+// var http = require('http'), 
+//     path = require('path'), 
+//     url = require('url');
+
+// var server; 
+
+// exports.start = function(port, callback) {
+//     server = http.createServer();
+//     server.on('request', function(req, res) {
+//         res.writeHead(200);
+//         res.write('200: Success\n');
+//         res.end();
+//     });
+// };
+
+// exports.stop = function(callback) {
+//     server.close(callback);
+// }
+
+// var firstServer = http.createServer(function(req, res) {
+//     var uri = url.parse(req.url, true).pathname;
+//     var query = url.parse(req.url, true);
+//     var filename = path.join(process.cwd(), 'static', uri);
+
+//     //DEBUG
+//     console.info('INFO - uri: ', uri);
+//     console.info('INFO - querystring: ', query.query);
+//     console.info('INFO - path or filename: ', filename);
+//     console.info('--------- ');
+
+//     if (uri === '/customer_scoring' && query) {
+//         res.writeHead(200, {'Content-Type': 'application/json'});
+//         res.write('200: Success\n');
+//         var payload = { 'propensity' : 0.26532, 'ranking' : 'C'};
+//         res.write(JSON.stringify(payload));
+//         res.end();
+//         return;
+//     } else {
+//         res.writeHead(404, {'Content-Type': 'text/plain'});
+//         res.write('404: File Not Found\n');
+//         res.end();
+//         return;
+//     }
+// });
+
+
+
+// YAKS!
+
+// var server = http.createServer();
+
+// exports.start = function(port, callback) {
+//     if (!port) {
+//         throw new Error('ERROR: Port is required');
+//     }
+
+//     server.on('request', function(req, res) {
+//         //DEBUG
+//         console.info('INFO - uri: ', uri);
+//         console.info('INFO - querystring: ', query.query);
+//         console.info('INFO - path or filename: ', filename);
+//         console.info('--------- ');
+
+//         if (uri === '/customer_scoring' && query) {
+//             res.writeHead(200, {'Content-Type': 'application/json'});
+//             res.write('200: Success\n');
+//             var payload = { 'propensity' : 0.26532, 'ranking' : 'C'};
+//             res.write(JSON.stringify(payload));
+//             res.end();
+//             return;
+//         } else {
+//             res.writeHead(404, {'Content-Type': 'text/plain'});
+//             res.write('404: File Not Found\n');
+//             res.end();
+//             return;
+//         }        
+//     })
+
+//     server.listen(port);
+
+//     if (callback) {
+//         callback();
+//     }
+// };
+
+// exports.stop = function(callback) {
+//     server.close(callback);
+// }
+
+
--- learning/sandbox/playing_with_lfo_api_exercise/server.js
+++ learning/sandbox/playing_with_lfo_api_exercise/server.js
@@ -2,157 +2,112 @@
 
 'use strict';
 
-var http = require('http');
+// Node 'Batteries Included'
+var http = require('http'),
+    url = require('url'),
+    util = require('util'),
+    querystring = require('querystring');
 
+// External Node 'Batteries'
 var nock = require('nock');
 
+// Mock Server
 var server = nock('http://not_real.com')
-          .persist()
-          .get('/customer_scoring/')
-          .reply(200, {
-            'propensity' : 0.26532,
-            'ranking' : 'C'
-          })
-          .get('/old_customer_scoring/')
-          .reply(302, 'Redirecting')
-          .get('/forbidden/')
-          .reply(403, 'Forbidden')
-          .get('/')
-          .reply(404, 'File Not Found');
-
-function getData(url) {
+            .persist()
   
+            .get('/customer_scoring/')
+            .reply(200, {
+              'propensity' : 0.26532,
+              'ranking' : 'C'
+            })
+  
+            .get('/old_customer_scoring/')
+            .reply(302, 'Redirecting')
+  
+            .get('/forbidden/')
+            .reply(403, 'Forbidden')
+  
+            .get('/')
+            .reply(404, 'File Not Found');
+
+// API Library
+var Api = function(url, path, qs) {
+    var self = this;
+    self.url = url;
+    self.path = path;
+    self.qs = qs;
+
+    if (!self.url) {
+        return new Error('ERR: URL is required.');
+    }
+
+    if (!self.path) {
+        return new Error('ERR: Path is required.')
+    }
+
+    if (!self.qs) {
+        return new Error('ERR: QueryString is required.');
+    }
+};
+
+Api.prototype.buildUrl = function() {
+    var self = this;
+    var qs = querystring.stringify(self.qs);
+    return url.resolve(self.url, self.path) + '?' + qs;
 }
 
-exports.test_200 = function(test) {
-    http.get('http://not_real.com/customer_scoring/', function(res) {
-        var s = '';
-        res.on('data', function(data) {
-            s += data;
-        });
-        res.on('end', function() {
-            test.equals(res.statusCode, 200, '200 Result');
-            test.equals(
-                s, 
-                {'propensity' : 0.26532, 'ranking' : 'C'}, 
-                '200 Result Content'
-            );
-            test.done();
-        });        
-    });
-};
+var endpoint = 'http://not_real.com';
+var path = '/customer_scoring/';
+var qs = {'income': 50000, 'zipcode': 60201, 'age': 35};
 
-exports.test_302 = function(test) {
-    http.get('http://not_real.com/old_customer_scoring/', function(res) {
-        test.equals(res.statusCode, 302);
-        test.done();
-    });
-};
-
-exports.test_403 = function(test) {
-    http.get('http://not_real.com/forbidden/', function(res) {
-        test.equals(res.statusCode, 403, '403 Result');
-        test.done();
-    });
-};
-
-exports.test_404 = function(test) {
-    http.get('http://not_real.com/', function(res) {
-        test.equals(res.statusCode, 404, '404 Result');
-        test.done();
-    });
-};
-
-
+var myApi = new Api(endpoint, path, qs);
+console.log(myApi.url);
+console.log(myApi.path);
+console.log(myApi.qs);
+console.log(myApi.buildUrl());
 
 server.isDone();
 
-// var http = require('http'), 
-//     path = require('path'), 
-//     url = require('url');
-
-// var server; 
-
-// exports.start = function(port, callback) {
-//     server = http.createServer();
-//     server.on('request', function(req, res) {
-//         res.writeHead(200);
-//         res.write('200: Success\n');
-//         res.end();
+// exports.test_200 = function(test) {
+//     http.get('http://not_real.com/customer_scoring/', function(res) {
+//         var s = '';
+//         res.on('data', function(data) {
+//             s += data;
+//         });
+//         res.on('end', function() {
+//             test.equals(res.statusCode, 200, '200 Result');
+//             test.equals(
+//                 s, 
+//                 {'propensity' : 0.26532, 'ranking' : 'C'}, 
+//                 '200 Result Content'
+//             );
+//             test.done();
+//         });        
 //     });
 // };
 
-// exports.stop = function(callback) {
-//     server.close(callback);
-// }
-
-// var firstServer = http.createServer(function(req, res) {
-//     var uri = url.parse(req.url, true).pathname;
-//     var query = url.parse(req.url, true);
-//     var filename = path.join(process.cwd(), 'static', uri);
-
-//     //DEBUG
-//     console.info('INFO - uri: ', uri);
-//     console.info('INFO - querystring: ', query.query);
-//     console.info('INFO - path or filename: ', filename);
-//     console.info('--------- ');
-
-//     if (uri === '/customer_scoring' && query) {
-//         res.writeHead(200, {'Content-Type': 'application/json'});
-//         res.write('200: Success\n');
-//         var payload = { 'propensity' : 0.26532, 'ranking' : 'C'};
-//         res.write(JSON.stringify(payload));
-//         res.end();
-//         return;
-//     } else {
-//         res.writeHead(404, {'Content-Type': 'text/plain'});
-//         res.write('404: File Not Found\n');
-//         res.end();
-//         return;
-//     }
-// });
-
-
-
-// YAKS!
-
-// var server = http.createServer();
-
-// exports.start = function(port, callback) {
-//     if (!port) {
-//         throw new Error('ERROR: Port is required');
-//     }
-
-//     server.on('request', function(req, res) {
-//         //DEBUG
-//         console.info('INFO - uri: ', uri);
-//         console.info('INFO - querystring: ', query.query);
-//         console.info('INFO - path or filename: ', filename);
-//         console.info('--------- ');
-
-//         if (uri === '/customer_scoring' && query) {
-//             res.writeHead(200, {'Content-Type': 'application/json'});
-//             res.write('200: Success\n');
-//             var payload = { 'propensity' : 0.26532, 'ranking' : 'C'};
-//             res.write(JSON.stringify(payload));
-//             res.end();
-//             return;
-//         } else {
-//             res.writeHead(404, {'Content-Type': 'text/plain'});
-//             res.write('404: File Not Found\n');
-//             res.end();
-//             return;
-//         }        
-//     })
-
-//     server.listen(port);
-
-//     if (callback) {
-//         callback();
-//     }
+// exports.test_302 = function(test) {
+//     http.get('http://not_real.com/old_customer_scoring/', function(res) {
+//         test.equals(res.statusCode, 302);
+//         test.done();
+//     });
 // };
 
-// exports.stop = function(callback) {
-//     server.close(callback);
-// }
+// exports.test_403 = function(test) {
+//     http.get('http://not_real.com/forbidden/', function(res) {
+//         test.equals(res.statusCode, 403, '403 Result');
+//         test.done();
+//     });
+// };
+
+// exports.test_404 = function(test) {
+//     http.get('http://not_real.com/', function(res) {
+//         test.equals(res.statusCode, 404, '404 Result');
+//         test.done();
+//     });
+// };
+
+
+
+
 
