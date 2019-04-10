1adb59fc08abb9d6af494cbc4c246865d62be43a
Terry Peppers <tpeppers@TerryPeppers.local>
2019-04-09 20:57:58 -0500

    With context.

--- learning/sandbox/playing_with_dates/dates.js
+++ learning/sandbox/playing_with_dates/dates.js
@@ -2,3 +2,10 @@
 let today = new Date().toDateString();
 console.log(today);
 
+function isToday(date) {
+    let today = new Date().toDateString;
+    return today === date.toDateString;
+}
+
+yesterday = new Date() - 1;
+console.log(isToday(yesterday));
